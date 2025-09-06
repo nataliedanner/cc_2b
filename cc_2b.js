8e5802be5a432c04e8e7a8a32806336bb5194289
Natalie Danner <danner15@usf.edu>
2025-09-06 12:51:51 -0400

    Initial Commit

---
+++ cc_2b.js
@@ -0,0 +1,12 @@
+let itemName = "Energy Drink";
+console.log(itemName);
+
+let currentStock = 100;
+let reorderLevel = 50;
+let targetStock = 200;
+let weeklyDemand = 75;
+supplierLeadTimeWeeks = 1;
+
+
+
+
