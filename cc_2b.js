7c8b448fc3b8284085883a6234298b0c9cdb92f6
Natalie Danner <danner15@usf.edu>
2025-09-06 13:08:10 -0400

    Added Some Inventory Metrics

--- cc_2b.js
+++ cc_2b.js
@@ -5,8 +5,9 @@ let currentStock = 100;
 let reorderLevel = 50;
 let targetStock = 200;
 let weeklyDemand = 75;
-supplierLeadTimeWeeks = 1;
+let supplierLeadTimeWeeks = 1;
 
+let weeksOfCover = weeklyDemand > 0 ? (currentStock / weeklyDemand): Infinity;
+console.log(weeksOfCover.toFixed(2));
 
-
-
+let stockDeficit = Math.max(0, targetStock-currentStock);
