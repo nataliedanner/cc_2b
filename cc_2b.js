00cb0ef6141ca0f2c403e3ec48eda60a132cc5d1
Natalie Danner <danner15@usf.edu>
2025-09-07 13:04:07 -0400

    Added Inventory Metrics and Added to Console

--- cc_2b.js
+++ cc_2b.js
@@ -1,15 +1,23 @@
 let itemName = "Energy Drink";
-console.log(itemName);
+console.log("Item Name:" , itemName);
 
-let currentStock = 100;
+let unitCost = 1.25
+let currentStock = 25;
 let reorderLevel = 50;
 let targetStock = 200;
 let weeklyDemand = 75;
 let supplierLeadTimeWeeks = 1;
 
 let weeksOfCover = weeklyDemand > 0 ? (currentStock / weeklyDemand): Infinity;
-console.log(weeksOfCover.toFixed(2));
+console.log("Weeks of Cover:" , weeksOfCover.toFixed(2));
 
 let stockDeficit = Math.max(0, targetStock-currentStock);
 
 let reorderQuantity = (currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks) ? Math.ceil(stockDeficit) : 0;
+console.log("Recommended Reorder Quantity:" , reorderQuantity);
+
+let estimatedReorderCost = reorderQuantity * unitCost;
+console.log("Estimated Reorder Cost: $" + estimatedReorderCost.toFixed(2));
+
+let reorderNow = currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks // true;
+console.log("Reorder Now?" , reorderNow);
